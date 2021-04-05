f6baecb19bedefc3e628f701465cef4d198b9068
gsgautam2492 <81697109+gsgautam2492@users.noreply.github.com>
2021-04-05 21:18:23 +0530

    Address Book Assignment changes to handle multiple Address Books.

--- src/com/techstack/training/streamApi/StreamApiDemo.java
+++ src/com/techstack/training/streamApi/StreamApiDemo.java
@@ -103,6 +103,10 @@ public class StreamApiDemo {
 		String address_book_name;
 		HashSet<contact> hs = new HashSet();
 
+		public HashSet<contact> getHs() {
+			return hs;
+		}
+
 		/**
 		 * @param address_book_name
 		 */
@@ -129,113 +133,167 @@ public class StreamApiDemo {
 
 		System.out.println("Welcome to Address Book Program \n \n");
 		Scanner keyboard = new Scanner(System.in);
-		String loopCheck = "";
-		address_book ab = new address_book("ab");
 
-		int choice = 0;
-		while (!loopCheck.equals("N") ||
-				!loopCheck.equals("n") ){
-
-			System.out.println("Please select from below options: \n" + "1. Add Contact to Address Book.\n"
-					+ "2. Update Contact to Address Book. \n" + "3. Delete Contact to Address Book. \n"
-					+ "4. Search Contact from Address book using City \n" + "5. Exit.\n \n" + "Value :");
-			choice = keyboard.nextInt();
+		HashSet<address_book> addBookList = new HashSet<address_book>();
+		String loopExit = "Y";
+		String loopCheck = "Y";
+		int count=0;
+		while (!loopExit.equals("N")) {
+			
+			System.out.println("Please enter below options: \n" + "1. Create New Address Book. \n"
+					+ "2. Add Contact to Existing Address Book. \n" + "3. Search across Address Books. \n"
+					+ "4. Exit.\n" + "Value :");
+			int choiceAddBook = 0;
+			int choice = 0;
+			//String loopCheck = "";
+			choiceAddBook = keyboard.nextInt();
 			keyboard.nextLine();
-			if (choice == 1) {
-				contact c1 = new contact();
-				System.out.print("\nPlease enter fname: ");
-				String fname = keyboard.nextLine(); // local variable
-				System.out.print("\nPlease enter lname: ");
-				String lname = keyboard.nextLine();
-				System.out.print("\nPlease enter addressLine1: ");
-				String addressLine1 = keyboard.nextLine();
-				System.out.print("\nPlease enter city: ");
-				String city = keyboard.nextLine();
-				System.out.print("\nPlease enter state: ");
-				String state = keyboard.nextLine();
-				System.out.print("\nPlease enter zip: ");
-				String zip = keyboard.nextLine();
-				System.out.print("\nPlease enter phone: ");
-				String phone = keyboard.nextLine();
-				System.out.print("\nPlease enter email: ");
-				String email = keyboard.nextLine();
-				c1.add(fname, lname, addressLine1, city, state, zip, phone, email);
-				// Boolean b =
-				List<contact> c = ab.hs.stream().filter(s -> s.fname.equals(fname))
-						.peek(s -> System.out.println("Record Exist " + s)).collect(Collectors.toList());
-				;
-				if (c.size() == 0) {
-					ab.hs.add(c1);
-				}
-			}
-
-			else if (choice == 2) {
-				System.out.println("\n Enter the First Name to Search");
-				String name = keyboard.nextLine();
-				int upChoice = 0;
-				System.out.println("\n Enter your choice: \n" + "1. Update fname.\n" + "2. Update lname.\n"
-						+ "3. Update phone.\n" + "4. Update email.\n" + "5. Update city.\n" + "6. Update state. \n"
-						+ "7. Update zip.\n" + "Value");
-				if (upChoice == 1) {
-					System.out.println("\n Enter the First Name to update");
-					String fname = keyboard.nextLine();
-					ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setFname(fname));
-				} else if (upChoice == 2) {
-					System.out.println("\n Enter the Last Name to update");
-					String lname = keyboard.nextLine();
-					ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setLname(lname));
-				} else if (upChoice == 3) {
-					System.out.println("\n Enter the phone to update");
-					String phone = keyboard.nextLine();
-					ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setPhone_no(phone));
-				} else if (upChoice == 4) {
-					System.out.println("\n Enter the email to update");
-					String email = keyboard.nextLine();
-					ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setEmail(email));
-				} else if (upChoice == 5) {
-					System.out.println("\n Enter the city to update");
-					String city = keyboard.nextLine();
-					ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setCity(city));
-				} else if (upChoice == 6) {
-					System.out.println("\n Enter the state to update");
-					String state = keyboard.nextLine();
-					ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setState(state));
-				} else if (upChoice == 7) {
-					System.out.println("\n Enter the zip to update");
-					String zip = keyboard.nextLine();
-					ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setZip_Code(zip));
-				} else if (upChoice == 8) {
-					System.out.println("\n Enter the Address Line1 to update");
-					String addressLine1 = keyboard.nextLine();
-					ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst()
-							.ifPresent(s -> s.setAddress_Line1(addressLine1));
-				}
-				// .anyMatch(s -> s.fname.equals(name))
-				;
-			}
-			
-			else if (choice ==3) {
-				System.out.println("\n Enter the First Name to Delete Contact");
-				String name = keyboard.nextLine();
-				//Predicate<Item> isQualified = s -> s.fname.equals(name);
-				//ab.hs.stream().filter(isQualified).findFirst();
-				ab.hs.removeIf(s -> s.fname.equals(name));
-			}
-			
-			else if (choice==4) {
+			address_book ab = new address_book();
+			if (choiceAddBook == 1) {
+				System.out.println("\n Enter the Address Book Name to Create");
+				String addBookName = keyboard.nextLine();
+				ab.setAddress_book_name(addBookName);
+				System.out.println("Please select from below options: \n" + "1. Add Contact to Address Book.\n"
+						+ "2. Update Contact to Address Book. \n" + "3. Delete Contact to Address Book. \n"
+						+ "4. Search Contact from Address book using City \n" + "5. Exit.\n \n" + "Value :");
+				choice = keyboard.nextInt();
+				keyboard.nextLine();
+				loopCheck="Y";
+				count=0;
+			} else if (choiceAddBook == 2) {
+				System.out.println("\n Enter the Address Book Name to Search");
+				String addBookName = keyboard.nextLine();
+				address_book ab1 = addBookList.stream().filter(s -> s.address_book_name.equals(addBookName))
+						.collect(Collectors.toList()).get(0);
+				ab = ab1;
+				System.out.println("Please select from below options: \n" + "1. Add Contact to Address Book.\n"
+						+ "2. Update Contact to Address Book. \n" + "3. Delete Contact to Address Book. \n"
+						+ "4. Search Contact from Address book using City \n" + "5. Exit.\n \n" + "Value :");
+				choice = keyboard.nextInt();
+				keyboard.nextLine();
+				loopCheck="Y";
+				count=0;
+			} else if (choiceAddBook == 3) {
 				System.out.println("\n Enter City to search contact");
 				String city = keyboard.nextLine();
-				List<contact> c=ab.hs.stream().filter(s -> s.city.equals(city)).collect(Collectors.toList());
-				System.out.println("Result : \n"+c.toString());
+				List<contact> c = addBookList.stream().flatMap(a -> a.getHs().stream()).filter(s -> s.city.equals(city))
+						.collect(Collectors.toList());
+				System.out.println("Result : \n" + c.toString());
 			}
-			
-			else if (choice==5) {
-				System.out.println("\n Exiting the program");
-				break;
+			else if (choiceAddBook == 4) {
+				break;			}
+			while (!loopCheck.equals("N")) {
+				if(count>0) {
+					System.out.println("Please select from below options: \n" + "1. Add Contact to Address Book.\n"
+							+ "2. Update Contact to Address Book. \n" + "3. Delete Contact to Address Book. \n"
+							+ "4. Search Contact from Address book using City \n" + "5. Exit.\n \n" + "Value :");
+					choice = keyboard.nextInt();
+					keyboard.nextLine();
+				}
+				if (choice == 1) {
+					contact c1 = new contact();
+					System.out.print("\nPlease enter fname: ");
+					String fname = keyboard.nextLine(); // local variable
+					System.out.print("\nPlease enter lname: ");
+					String lname = keyboard.nextLine();
+					System.out.print("\nPlease enter addressLine1: ");
+					String addressLine1 = keyboard.nextLine();
+					System.out.print("\nPlease enter city: ");
+					String city = keyboard.nextLine();
+					System.out.print("\nPlease enter state: ");
+					String state = keyboard.nextLine();
+					System.out.print("\nPlease enter zip: ");
+					String zip = keyboard.nextLine();
+					System.out.print("\nPlease enter phone: ");
+					String phone = keyboard.nextLine();
+					System.out.print("\nPlease enter email: ");
+					String email = keyboard.nextLine();
+					c1.add(fname, lname, addressLine1, city, state, zip, phone, email);
+					// Boolean b =
+					List<contact> c = ab.hs.stream().filter(s -> s.fname.equals(fname))
+							.peek(s -> System.out.println("Record Exist " + s)).collect(Collectors.toList());
+					;
+					if (c.size() == 0) {
+						ab.hs.add(c1);
+					}
+				}
+
+				else if (choice == 2) {
+					System.out.println("\n Enter the First Name to Search");
+					String name = keyboard.nextLine();
+					int upChoice = 0;
+					System.out.println("\n Enter your choice: \n" + "1. Update fname.\n" + "2. Update lname.\n"
+							+ "3. Update phone.\n" + "4. Update email.\n" + "5. Update city.\n" + "6. Update state. \n"
+							+ "7. Update zip.\n" + "Value");
+					if (upChoice == 1) {
+						System.out.println("\n Enter the First Name to update");
+						String fname = keyboard.nextLine();
+						ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setFname(fname));
+					} else if (upChoice == 2) {
+						System.out.println("\n Enter the Last Name to update");
+						String lname = keyboard.nextLine();
+						ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setLname(lname));
+					} else if (upChoice == 3) {
+						System.out.println("\n Enter the phone to update");
+						String phone = keyboard.nextLine();
+						ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst()
+								.ifPresent(s -> s.setPhone_no(phone));
+					} else if (upChoice == 4) {
+						System.out.println("\n Enter the email to update");
+						String email = keyboard.nextLine();
+						ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setEmail(email));
+					} else if (upChoice == 5) {
+						System.out.println("\n Enter the city to update");
+						String city = keyboard.nextLine();
+						ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setCity(city));
+					} else if (upChoice == 6) {
+						System.out.println("\n Enter the state to update");
+						String state = keyboard.nextLine();
+						ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setState(state));
+					} else if (upChoice == 7) {
+						System.out.println("\n Enter the zip to update");
+						String zip = keyboard.nextLine();
+						ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst().ifPresent(s -> s.setZip_Code(zip));
+					} else if (upChoice == 8) {
+						System.out.println("\n Enter the Address Line1 to update");
+						String addressLine1 = keyboard.nextLine();
+						ab.hs.stream().filter(s -> s.fname.equals(name)).findFirst()
+								.ifPresent(s -> s.setAddress_Line1(addressLine1));
+					}
+					// .anyMatch(s -> s.fname.equals(name))
+					;
+				}
+
+				else if (choice == 3) {
+					System.out.println("\n Enter the First Name to Delete Contact");
+					String name = keyboard.nextLine();
+					// Predicate<Item> isQualified = s -> s.fname.equals(name);
+					// ab.hs.stream().filter(isQualified).findFirst();
+					ab.hs.removeIf(s -> s.fname.equals(name));
+				}
+
+				else if (choice == 4) {
+					System.out.println("\n Enter City to search contact");
+					String city = keyboard.nextLine();
+					List<contact> c = ab.hs.stream().filter(s -> s.city.equals(city)).collect(Collectors.toList());
+					System.out.println("Result : \n" + c.toString());
+				}
+
+				else if (choice == 5) {
+					System.out.println("\n Exiting the program");
+					break;
+				}
+				count++;
+				System.out.println("\nDo you want to continue:(Y/N) ");
+				loopCheck = keyboard.nextLine();
 			}
-			System.out.print("\nDo you want to continue:(Y/N) ");
-			loopCheck = keyboard.nextLine();
+			System.out.println(ab.hs.toString());
+			String name=ab.address_book_name;
+			addBookList.removeIf(s->s.address_book_name.equals(name));
+			addBookList.add(ab);
+			System.out.println("\nDo you want to continue:(Y/N) ");
+			loopExit = keyboard.nextLine();
 		}
-		System.out.print(ab.hs.toString());
+		//System.out.println(addBookList.stream().toString());
 	}
 }
